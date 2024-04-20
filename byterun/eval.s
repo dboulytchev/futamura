c933dceb644fa5e8d7dc86e9b97bdee133ef83f7
blonded04 <generalretcher@gmail.com>
2024-04-20 14:50:48 +0300

    added bops

--- byterun/eval.s
+++ byterun/eval.s
@@ -1,4 +1,16 @@
 # Various macro definitions
+# fixnum arithmetics: make box for an integer
+	.macro FIX_BOX dst
+	sall 	$1, \dst
+	xorl 	$1, \dst
+	.endm
+
+# fixnum arithmetics: unbox integer
+	.macro FIX_UNB dst
+	xorl 	$1, \dst
+	sarl 	$1, \dst
+	.endm
+
 	.macro	POP dst
 	subl	$4, %esi
 	movl	(%esi), \dst
@@ -78,25 +90,109 @@ eval:
 	ret
 
 b_add:	POP2 	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB %ebx
 	addl	%ebx, %eax
+	FIX_BOX %eax
 	PUSH	%eax
 
 b_sub:	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB %ebx
 	subl	%eax, %ebx
+	FIX_BOX %ebx
 	PUSH	%ebx
 
 b_mul:	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB %ebx
 	imul	%ebx
+	FIX_BOX %eax
 	PUSH	%eax
 
 b_div:	POP2	%ebx %eax
+	FIX_UNB %eax
+	FIX_UNB %ebx
 	cltd
 	idiv	%ebx
+	FIX_BOX %eax
 	PUSH	%eax
 
 b_mod:	POP2	%ebx %eax
+	FIX_UNB %eax
+	FIX_UNB %ebx
 	cltd
 	idiv	%ebx
+	FIX_BOX %edx
 	PUSH	%edx
-	
-binops:	.int b_add,b_sub,b_mul,b_mod
+
+b_eq: 	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB %ebx
+	xorl	%edx, %edx
+	cmpl	%eax, %ebx
+	xorl 	%eax, %eax
+	seteb	%dl
+	FIX_BOX %edx
+	PUSH 	%edx
+
+b_neq: 	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB %ebx
+	xorl	%edx, %edx
+	cmpl	%eax,%ebx
+	setneb	%dl
+	FIX_BOX %edx
+	PUSH 	%edx
+
+b_lt: 	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB	%ebx
+	xorl	%edx, %edx
+	cmpl	%eax, %ebx
+	setlb	%dl
+	FIX_BOX %edx
+	PUSH	%edx
+
+b_le:	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB	%ebx
+	xorl	%edx, %edx
+	cmpl	%eax, %ebx
+	setleb	%dl
+	FIX_BOX %edx
+	PUSH	%edx
+
+b_gt: 	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB	%ebx
+	xorl	%edx, %edx
+	cmpl	%eax, %ebx
+	setgb	%dl
+	FIX_BOX %edx
+	PUSH	%edx
+
+b_ge:	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB	%ebx
+	xorl	%edx, %edx
+	cmpl	%eax, %ebx
+	setgeb	%dl
+	FIX_BOX %edx
+	PUSH	%edx
+
+b_and:	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB	%ebx
+	andl	%eax, %ebx
+	FIX_BOX %ebx
+	PUSH	%ebx
+
+b_or:	POP2	%eax %ebx
+	FIX_UNB %eax
+	FIX_UNB	%ebx
+	orl		%eax, %ebx
+	FIX_BOX %ebx
+	PUSH	%ebx
+
+binops:	.int b_add,b_sub,b_mul,b_div,b_mod
